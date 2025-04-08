4c86da35c8f49838ea75793e487e47ceba7731c0
Shivank Kacker <kacker.shivank@gmail.com>
2025-04-08 22:54:22 +0530

    added google analytics

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,12 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react-swc'
+import { VitePluginRadar } from "vite-plugin-radar";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [react(), VitePluginRadar({
+    analytics: {
+      id: "G-NNWHEQDQJP",
+    },
+  }),],
 })
